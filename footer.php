aa52080dc7cbc0675990e50df13120235def1b98
Sam Garg <samgarg86@gmail.com>
2016-06-09 21:06:11 -0500

    Footer padding - place shopping cart icon under "as seen on tv" - Add copyright and brand text to footer - Checkout form Country/State drop down fonts arial (capitalise) - SHIP TO A DIFFERENT ADDRESS? -> arial

--- footer.php
+++ footer.php
@@ -1,13 +1,19 @@
-<section id="footer">
+<section id="footer" class="spacer-upper-30">
     <div class="container-fluid black">
-    <?php
-        wp_nav_menu(array(
-            'theme_location' => 'footer_navigation', // menu slug from step 1
-            'container' => false, // 'div' container will not be added
-            'menu_class' => 'nav', // <ul class="nav">
-            'fallback_cb' => false // name of default function from step 2
-        ));
-    ?>
+        <div class="row">
+            <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer_navigation', // menu slug from step 1
+                    'container' => false, // 'div' container will not be added
+                    'menu_class' => 'nav', // <ul class="nav">
+                    'fallback_cb' => false // name of default function from step 2
+                ));
+            ?>
+        </div>
+        <div class="row footer-text">
+            Outdoor Brands, LLC, PO Box 8464, Jupiter, Florida 33468 USA, 561.200.5958<br>
+            &copy;Outdoor Brands, LLC All Rights Reserved
+        </div>
     </div>
 </section>
 <?php wp_footer(); ?>
